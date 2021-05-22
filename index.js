12c83f3654e5896bf0a39a59fbdff6370ec5a6ff
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-22 10:44:49 -0500

    added require to index js for lib

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const inquirer = require('inquirer')
 
 const generateIndex = require('./src/generateHTML')
 
-//class 
+const Employee =require('./lib/Employee')
+const Engineer =require('./lib/Engineer')
+const Intern =require('./lib/Intern')
+const Manager =require('./lib/Manager')
+
+let employeeArray = [];
+//class  creations 
+class User {
+    constructor() {
+
+    }
+}
