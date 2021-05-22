329b39d3b6429c46def9d16c81be82d5f7ff35d4
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-22 10:31:12 -0500

    boilerplate html added to function generatehtml in file of src

---
+++ src/generateHTML.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+
+// var filename = //path to file
+// var createfile = fs.createWrite
+
+function generateHTML(info) {
+    return `<html lang="en">
+    <head>
+      <meta charset="utf-8">
+    
+      <title>The HTML5 Herald</title>
+      <meta name="description" content="The HTML5 Herald">
+      <meta name="author" content="SitePoint">
+    
+      <link rel="stylesheet" href="css/styles.css?v=1.0">
+    
+    </head>
+    
+    <body>
+      <script src="js/scripts.js"></script>
+    </body>
+    </html>`
+}
