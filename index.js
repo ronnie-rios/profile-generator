2d9f3624b27eb4925ba0a1267d8f315e005acbbc
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-29 20:45:50 -0500

    added engineer prompts and object creation

--- index.js
+++ index.js
@@ -26,14 +26,16 @@ function addMember() {
             managerInformation();
         } else if(memberRole === 'Engineer') {
             //function for engineer
+            engineerInformation();
         } else if (memberRole === 'Intern') {
             //function for intern
         } else {
             //create the team
         }
     });
-}
+};
 
+//manager creation and prompts
 function managerInformation() {
     inquirer.prompt([
         {
@@ -63,6 +65,40 @@ function managerInformation() {
             data.managerNumber,
             data.managerEmail);
         teamArray.push(manager);
+        addMember();
+    });
+};
+
+//engineer creation and prompts
+function engineerInformation() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "Enter the engineers's name",
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: 'Enter your employee ID#',
+        },
+        {
+            type: 'input',
+            name: 'engineerProfile',
+            message: 'Emter your github profile username.'
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Enter your email address'
+        }
+    ]).then(function(data) {
+        const engineer = new Engineer(
+            data.engineerName,
+            data.engineerId,
+            data.engineerProfile,
+            data.engineerEmail);
+        teamArray.push(engineer);
         addMember();
     });
 };
