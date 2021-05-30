b3d1d6f14af615f9f64d48164727fc2b3341552e
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-30 08:20:02 -0500

    added intern prompts

--- index.js
+++ index.js
@@ -29,75 +29,110 @@ function addMember() {
             engineerInformation();
         } else if (memberRole === 'Intern') {
             //function for intern
+            internInformation();
         } else {
             //create the team
         }
     });
 };
 
-//manager creation and prompts
+//manager prompts
 function managerInformation() {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'managerName',
+            name: 'name',
             message: "Enter the manager's name",
         },
         {
             type: 'input',
-            name: 'managerID',
+            name: 'id',
             message: 'Enter your employee ID#',
         },
         {
             type: 'input',
-            name: 'managerNumber',
-            message: 'Emter your office number.'
+            name: 'number',
+            message: 'Enter your office number.'
         },
         {
             type: 'input',
-            name: 'managerEmail',
+            name: 'email',
             message: 'Enter your email address'
         }
     ]).then(function(data) {
         const manager = new Manager(
-            data.managerName,
-            data.managerId,
-            data.managerNumber,
-            data.managerEmail);
+            data.name,
+            data.id,
+            data.number,
+            data.email);
         teamArray.push(manager);
         addMember();
     });
 };
 
-//engineer creation and prompts
+//engineer prompts
 function engineerInformation() {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'engineerName',
+            name: 'name',
             message: "Enter the engineers's name",
         },
         {
             type: 'input',
-            name: 'engineerID',
+            name: 'id',
             message: 'Enter your employee ID#',
         },
         {
             type: 'input',
-            name: 'engineerProfile',
-            message: 'Emter your github profile username.'
+            name: 'github',
+            message: 'Enter your github profile username.'
         },
         {
             type: 'input',
-            name: 'managerEmail',
-            message: 'Enter your email address'
+            name: 'email',
+            message: 'Enter their email address'
         }
     ]).then(function(data) {
         const engineer = new Engineer(
-            data.engineerName,
-            data.engineerId,
-            data.engineerProfile,
-            data.engineerEmail);
+            data.name,
+            data.id,
+            data.github,
+            data.email);
+        teamArray.push(engineer);
+        addMember();
+    });
+};
+
+//intern prompts
+function internInformation() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the intern's name",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter their employee ID#',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the intern's school."
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'Enter their email address'
+        }
+    ]).then(function(data) {
+        const engineer = new Engineer(
+            data.name,
+            data.id,
+            data.school,
+            data.email);
         teamArray.push(engineer);
         addMember();
     });
