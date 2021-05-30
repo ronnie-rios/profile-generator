f36f5071eb49d0bc7e1a49b648a5ddeee021b4f8
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-30 12:25:58 -0500

    added html boilerplate

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -66,3 +66,79 @@ const generateIntern = function(intern) {
   </div>
   `;
 }
+
+generateHTML = (data) => {
+
+  // array for cards 
+  htmlArray = []; 
+
+  for (let i = 0; i < data.length; i++) {
+      const employee = data[i];
+      const role = employee.getRole(); 
+
+
+      // call manager function
+      if (role === 'Manager') {
+          const managerCard = generateManager(employee);
+
+          htmlArray.push(managerCard);
+      }
+
+      // call engineer function
+      if (role === 'Engineer') {
+          const engineerCard = generateEngineer(employee);
+
+          htmlArray.push(engineerCard);
+      }
+
+      // call intern function 
+      if (role === 'Intern') {
+          const internCard = generateIntern(employee);
+
+          htmlArray.push(internCard);
+      }
+      
+  }
+
+  // joining strings 
+  const employeeCards = htmlArray.join('')
+
+  // return to generated page
+  const generateCards= generateTeamHtml(employeeCards); 
+  return generateCards;
+
+}
+
+const generateTeamHtml = function(cards) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+      <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css"/>
+          <link rel="stylesheet" href="style.css"/>
+  </head>
+  <body>
+      <header>
+          <nav class="navbar" id="navbar">
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <div class="container">
+              <div class="row justify-content-center" id="team-cards">
+                  <!--Team Cards-->
+                  ${cards}
+              </div>
+          </div>
+      </main>
+      
+  </body>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src=src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc"crossorigin="anonymous></script>
+  </html>
+  `
+}
